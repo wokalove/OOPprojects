8d7c6a9b189ca6544fa3e5036154739778fe69cf
wokalove <oladuda1999@gmail.com>
2020-10-11 19:36:23 +0200

    up

--- lab1/Data.py
+++ lab1/Data.py
@@ -4,7 +4,7 @@ import xmltodict
 class Connect:
     def __init__(self,url_link):
         self.connect = urllib.request.urlopen(url_link)
-        #Parsing(selff)
+        #Parsing(self)
 
 class Parsing:
     def __init__(self,obj):
@@ -16,39 +16,59 @@ class CurrencySet:
     def __init__(self,obj):
         self.currency_set = obj.data['tabela_kursow']['pozycja']
         #print(self.currency_set)
-
-class Currency:
-    def get_position(self,obj):
-        self.len_currency_set = len(obj.currency_set)
+    def get_position(self):
+        all_positions = []
+        self.len_currency_set = len(self.currency_set)
         for i in range(self.len_currency_set):
-            self.result_list = [v for k,v in obj.currency_set[i].items()]
-            print(self.result_list)
-            return self.result_list
+            self.result_list = [v for k,v in self.currency_set[i].items()]
+            #all_positions.append(self.result_list)
+            #print(self.result_list)
+        return all_positions
     
-    def get_currency_name(self,obj):
-        values = self.get_position(obj)
-        name = values[0]
+class Currency(object):
+    
+    def get_currency_name(self,curr,name):
+         
+        self.len_currency_set = len(curr.currency_set)
+        for i in range(self.len_currency_set):
+            self.result_list = [v for k,v in curr.currency_set[i].items()]
+            if self.result_list[0] == name:
+                name = self.result_list[0]
         return name
-    def get_converter(self,obj):
-        values = self.get_position(obj)
-        converters = values[1]
-        return converters
-    def get_currency_code(self,obj):
-        values = self.get_position(obj)
-        codes = values[2]
-        return codes
-    def get_currency_rate(self,from_curr,to_curr):
-        values = self.result_list
-        
-        rates = values[3]
-        return rates
 
+    def get_converter(self,curr,name):
+        self.len_currency_set = len(curr.currency_set)
+        for i in range(self.len_currency_set):
+            self.result_list = [v for k,v in curr.currency_set[i].items()]
 
+            if self.result_list[0] == name:
+                converter = self.result_list[1]
+        return converter
 
+    def get_currency_code(self,curr,name):
+        self.len_currency_set = len(curr.currency_set)
+        for i in range(self.len_currency_set):
+            self.result_list = [v for k,v in curr.currency_set[i].items()]
+    
+            if self.result_list[0] == name:
+                code = self.result_list[2]
+        return code
+    def get_currency_rate(self,curr,from_curr,to_curr):
+        rate = []
+        self.len_currency_set = len(curr.currency_set)
+        for i in range(self.len_currency_set):
+            self.result_list = [v for k,v in curr.currency_set[i].items()]
+            if self.result_list[0] == from_curr or self.result_list[0] == to_curr:
+                rate.append(self.result_list[3])
+        #print(rate)
+        return rate
+
+'''
 url='https://www.nbp.pl/kursy/xml/lasta.xml'
 
 object1 = Connect(url)
 parsed = Parsing(object1)
 currency_set = CurrencySet(parsed)
 currency = Currency()
-print(currency.get_position(currency_set))
+print(currency.get_currency_code(currency_set,'peso filipińskie'))
+'''
--- lab1/Interface.py
+++ lab1/Interface.py
@@ -6,13 +6,27 @@ class UserInterface(object):
         self.business_logic = Convert()
         self.data = Currency()
     def questions(self):
-        self.money = int(input("Enter money amount:"))
-        self.from_currency = input("From currency:")
-        self.to_currency = input("To currency:")
+        
+        url='https://www.nbp.pl/kursy/xml/lasta.xml'
 
-        rates = self.data.get_currency_rate(self.from_currency,self.to_currency)
-        rate_one = 1
-        rate_two = 2
+        object1 = Connect(url)
+        parsed = Parsing(object1)
+        currency_set = CurrencySet(parsed)
+        currency = Currency()
+        
+
+        self.money = int(input("Enter money amount:"))
+        #self.from_curr = input("From currency:")
+        #self.to_curr = input("To currency:")
+        self.from_curr = 'peso filipińskie'
+        self.to_curr = 'frank szwajcarski'
+        rate = currency.get_currency_rate(currency_set,self.from_curr,self.to_curr)
+        #print(rate)
+        #rates = self.data.get_currency_rate(self.from_currency,self.to_currency)
+        
+        rate_one = rate[0]
+        rate_two = rate[1]
+        print("1:",rate_one,"2:",rate_two)
         converter_one = 1
         converter_two = 2
     

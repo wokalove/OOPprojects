e47694591583e4d7657083f9b0431542984e6eae
wokalove <oladuda1999@gmail.com>
2020-10-11 15:12:32 +0200

    up

--- lab1/BusinessLogic.py
+++ lab1/BusinessLogic.py
@@ -2,17 +2,19 @@ from Data import *
 
 class Convert(object):
     @staticmethod
-    def convert(money,from_currency, to_currency):
-        print(money,from_currency,to_currency)
-        
-       
-'''
-        initial_amount = amount 
-        if from_currency != 'EUR' : 
-            amount = amount / self.rates[from_currency] 
-  
-        # limiting the precision to 2 decimal places 
-        amount = round(amount * self.rates[to_currency], 2) 
-        print('{} {} = {} {}'.format(initial_amount, from_currency, amount, to_currency)) 
-'''
+    def convert_from_pln(money,avg_exchange_rate,converter):
+        converted = money* avg_exchange_rate/converter
+        return converted
+    @staticmethod
+    def convert_to_pln(money,avg_exchange_rate,converter):
+        converted = money/avg_exchange_rate*converter
+        return converted
+    
+    def convert(self,money,currency_one, currency_two,converter_one, converter_two):
+        to_pln = self.convert_to_pln(money, currency_one,converter_one)
+        converted_money = self.convert_from_pln(to_pln,currency_two,converter_two)
+        return converted_money
 
+
+con = Convert()
+print(con.convert_to_pln(20,4.30,100))
--- lab1/Interface.py
+++ lab1/Interface.py
@@ -1,12 +1,17 @@
 from BusinessLogic import *
 
 class UserInterface(object):
+    def __init__(self) -> None:
+        self.business_logic = Convert()
     def questions(self):
         self.money = input("Enter money amount:")
-        self.from_currency = input("From currency:")
-        self.to_currency = input("To currency:")
+        self.currency_one = input("From currency:")
+        self.currency_two = input("To currency:")
+        #Currency.get_rate()
+        converter_one = 1
+        converter_two = 2
     
-        Convert.convert(self.money,self.from_currency,self.to_currency)
+        self.business_logic.convert(self.money,self.currency_one,self.currency_two,converter_one,converter_two)
 
 interface = UserInterface()
 interface.questions()

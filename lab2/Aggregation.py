2659be146dfe9df0f20d218186e6b8eaae730aa7
wokalove <oladuda1999@gmail.com>
2020-10-20 11:57:30 +0200

    up

--- lab2/Aggregation.py
+++ lab2/Aggregation.py
@@ -23,7 +23,7 @@ class Vector2D(IVector):
         self._y = y
         
     def Abs(self):
-        return math.sqrt(pow(self._x,2)+pow(self._y,2))
+        return round(math.sqrt(pow(self._x,2)+pow(self._y,2)),2)
         
     def getComponents(self):
        return[self._x,self._y]
@@ -34,7 +34,7 @@ class Vector2D(IVector):
 
         cos = (self._x*ox._x + self._y*ox._y)/self.Abs()*abs_ox
         angle = math.degrees(math.acos(cos))
-        return angle
+        return round(angle,2)
 
     def cdot(self, vector):
         dot_product = self._x *vector._x + self._y*vector._y
@@ -50,7 +50,7 @@ class Vector3D(IVector):
         self.__z = z
    def Abs(self):
        x, y = self.__vector.getComponents()
-       return math.sqrt(pow(x,2)+pow(y,2)+pow(self.__z,2))
+       return round(math.sqrt(pow(x,2)+pow(y,2)+pow(self.__z,2)),2)
 
    def getComponents(self):
        x, y = self.__vector.getComponents()
@@ -61,14 +61,14 @@ class Vector3D(IVector):
        fi = math.atan(y/x)
        psi = math.atan(math.sqrt(pow(x,2)+ pow(y,2))/self.__z)
        
-       angles = [fi, psi]
+       angles = [round(fi,2),round(psi,2)]
        return angles
 
    def cdot(self, vector):
        x, y = self.__vector.getComponents()
        x_v,y_v,z_v = vector.getComponents()
        dot_product = x *x_v + y *y_v +self.__z *z_v
-       return dot_product
+       return round(dot_product,2)
 
 
    def spherical_coordinates(self):
@@ -90,27 +90,72 @@ class Decorator(Vector3D):
         z_v = x*y_2 - (y *x_2)
         new_vector = Vector3D(x_v,y_v,z_v)
         return new_vector
-class Adapter2D:
-    pass
-    
+
+class VectorAdapter2D:
+    @abstractmethod
+    def from_polar_to_cartesian(self):
+        pass
+class Adapter2D(VectorAdapter2D):
+    def __init__(self,r,angle):
+        self.r = r
+        self.angle = angle
+    def from_polar_to_cartesian(self):
+        x = self.r*math.cos(math.radians(self.angle))
+        y = self.r*math.sin(math.radians(self.angle))
+        return [round(x,2),round(y,2)]
+
+   
+class VectorAdapter3D:
+    @abstractmethod
+    def spherical_coordinate_to_polar(self):
+        pass
+class Adapter3D(VectorAdapter3D):
+    def __init__(self,r,psi,fi):
+        self.__r = r
+        self.__psi = psi
+        self.__fi = fi
+    def spherical_coordinate_to_polar(self):
+       x = self.__r* math.sin(math.radians(self.__psi))*math.cos(math.radians(self.__fi))
+       y = self.__r*math.sin(math.radians(self.__psi))*math.sin(math.radians(self.__fi))
+       z = self.__r* math.cos(math.radians(self.__psi))
+       return [round(x,2),round(y,2),round(z,2)]
+
         
-v2= Vector2D(1,1)
-v2_2 = Vector2D(1,2)
 
-print(v2.Abs())
-print(v2.getAngles())
-'''
-print("DOOOt",v2.cdot(v2_2))
+def main():
+    v2 = Vector2D(1,1)
+    v2_2 = Vector2D(1,2)
+    print("Abs v2:",v2.Abs(), "| Abs v2_2:",v2_2.Abs())
+    print("Components v2:",v2.getComponents(),"| Components v2_2:",v2_2.getComponents())
+    print("Angles v2:", v2.getAngles(),"| Angles v2_2:",v2_2.getAngles())
+    print("Scalar dot of v2, v2_2:",v2.cdot(v2_2))
 
-v3= Vector3D(1,3,5)
-v3_2 =Vector3D(1,2,7)
-print(v3.getAngles())
-print(v3.cdot(v3_2))
-print(v3.cross_product(v3_2))
-'''
-deco = Decorator(1,3,5)
-v3_2 = Vector3D(1,2,7)
-print(deco.cross_product(v3_2).getComponents())
 
-v3= Vector3D(1,3,5)
-print(v3.getAngles())
+    v3 = Vector3D(1,3,5)
+    v3_2 = Vector3D(1,2,7)
+    decorator = Decorator(1,3,5)
+    print("Abs v3:",v3.Abs(), "| Abs v3_2:",v3_2.Abs())
+    print("Components v3:",v3.getComponents(),"| Components v3_2:",v3_2.getComponents())
+    print("Angles v3:", v3.getAngles(),"| Angles v3_2:",v3_2.getAngles())
+    print("Scalar dot of v3, v3_2:",v3.cdot(v3_2))
+    print("Cross product:",decorator.cross_product(v3_2).getComponents())
+
+
+
+    a_2D = Adapter2D(3,30)
+    print("From polar to cartesian:",a_2D.from_polar_to_cartesian())
+    a_3D = Adapter3D(2,30,30)
+    print("From spherical to cartesian:",a_3D.spherical_coordinate_to_polar())
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
--- lab2/Inheritance.py
+++ lab2/Inheritance.py
@@ -23,7 +23,7 @@ class Vector2D(IVector):
         self.__y = y
         
     def Abs(self):
-        return math.sqrt(pow(self.__x,2)+pow(self.__y,2))
+        return round(math.sqrt(pow(self.__x,2)+pow(self.__y,2)),2)
         
     def getComponents(self):
        return[self.__x,self.__y]
@@ -34,11 +34,11 @@ class Vector2D(IVector):
 
         cos = (self.__x*ox.__x + self.__y*ox.__y)/self.Abs()*abs_ox
         angle = math.degrees(math.acos(cos))
-        return angle
+        return round(angle,2)
 
     def cdot(self, vector):
         dot_product = self.__x *vector.__x + self.__y*vector.__y
-        return dot_product
+        return round(dot_product,2)
     
         
 class Vector3D(Vector2D):
@@ -47,7 +47,7 @@ class Vector3D(Vector2D):
         self.__z = z
    def Abs(self):
        x,y = super().getComponents()
-       return math.sqrt(pow(x,2)+pow(y,2)+pow(self.__z,2))
+       return round(math.sqrt(pow(x,2)+pow(y,2)+pow(self.__z,2)),2)
 
    def getComponents(self):
        x,y = super().getComponents()
@@ -59,7 +59,7 @@ class Vector3D(Vector2D):
        fi = math.atan(y/x)
        psi = math.atan(math.sqrt(pow(x,2)+ pow(y,2))/self.__z)
        
-       angles = [fi, psi]
+       angles = [round(fi,2), round(psi,2)]
        return angles
        
 
@@ -67,49 +67,75 @@ class Vector3D(Vector2D):
        x,y = super().getComponents()
        x_v,y_v,z_v = vector.getComponents()
        dot_product = x *x_v + y*y_v +self.__z *z_v
-       return dot_product
+       return round(dot_product,2)
 
    def cross_product(self,vector):
        x_v,y_v,z_v = vector.getComponents()
        x,y = super().getComponents()
 
-       x = y*z_v - (self.__z*y_v)
-       y = self.__z *x_v - (self.__x *z_v)
-       z = x*y_v - (y *x_v)
-       new_vector = [x,y,z]
+       x_p = y*z_v - (self.__z*y_v)
+       y_p = self.__z *x_v - (x *z_v)
+       z_p = x*y_v - (y *x_v)
+       new_vector = [round(x_p,2),round(y_p,2),round(z_p,2)]
 
        
        return new_vector
 
-class Adapter2D:
-    #def __init__(self,angle,r):
+
+class VectorAdapter2D:
+    @abstractmethod
     def from_polar_to_cartesian(self):
-        x = r*math.cos(angle)
-        y = r*math.sin(angle)
-        return [x,y]
+        pass
+class Adapter2D(VectorAdapter2D):
+    def __init__(self,r,angle):
+        self.r = r
+        self.angle = angle
+    def from_polar_to_cartesian(self):
+        x = self.r*math.cos(math.radians(self.angle))
+        y = self.r*math.sin(math.radians(self.angle))
+        return [round(x,2),round(y,2)]
 
    
-class Adapter3D:
-    
-    def spherical_coordinates(self,fi,psi,r):
-       x = r* math.cos(psi)*math.cos(fi)
-       y = r*math.cos(psi)*math.sin(fi)
-       z = r* math.sin(psi)
-       return [x,y,z]
+class VectorAdapter3D:
+    @abstractmethod
+    def spherical_coordinate_to_polar(self):
+        pass
+class Adapter3D(VectorAdapter3D):
+    def __init__(self,r,psi,fi):
+        self.__r = r
+        self.__psi = psi
+        self.__fi = fi
+    def spherical_coordinate_to_polar(self):
+       x = self.__r* math.sin(math.radians(self.__psi))*math.cos(math.radians(self.__fi))
+       y = self.__r*math.sin(math.radians(self.__psi))*math.sin(math.radians(self.__fi))
+       z = self.__r* math.cos(math.radians(self.__psi))
+       return [round(x,2),round(y,2),round(z,2)]
+
+def main():
+    v2 = Vector2D(1,1)
+    v2_2 = Vector2D(1,2)
+    print("Abs v2:",v2.Abs(), "| Abs v2_2:",v2_2.Abs())
+    print("Components v2:",v2.getComponents(),"| Components v2_2:",v2_2.getComponents())
+    print("Angles v2:", v2.getAngles(),"| Angles v2_2:",v2_2.getAngles())
+    print("Scalar dot of v2, v2_2:",v2.cdot(v2_2))
+
+
+    v3 = Vector3D(1,3,5)
+    v3_2 = Vector3D(1,2,7)
+    print("Abs v3:",v3.Abs(), "| Abs v3_2:",v3_2.Abs())
+    print("Components v3:",v3.getComponents(),"| Components v3_2:",v3_2.getComponents())
+    print("Angles v3:", v3.getAngles(),"| Angles v3_2:",v3_2.getAngles())
+    print("Scalar dot of v3, v3_2:",v3.cdot(v3_2))
+    print("Cross product:",v3.cross_product(v3_2))
+
+
+    a_2D = Adapter2D(3,30)
+    print("From polar to cartesian:",a_2D.from_polar_to_cartesian())
+    a_3D = Adapter3D(2,30,30)
+    print("From spherical to cartesian:",a_3D.spherical_coordinate_to_polar())
+
+if __name__ == "__main__":
+    main()
 
 
 
-v2= Vector2D(1,1)
-v2_2 = Vector2D(1,2)
-
-print(v2.Abs())
-print(v2.getAngles())
-
-print("DOOOt",v2.cdot(v2_2))
-
-v3= Vector3D(1,3,5)
-v3_2 =Vector3D(1,2,7)
-print(v3.Abs())
-print(v3.getAngles())
-#print(v3.cdot(v3_2))
-#print(v3.cross_product(v3_2))
